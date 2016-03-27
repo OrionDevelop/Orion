66bc0cad2ef8ef48572b2fceaf2952f40d71dcba
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-03-27 15:57:25 +0900

    Update for UWP project

--- Orion/Properties/AssemblyInfo.cs
+++ Orion/Properties/AssemblyInfo.cs
@@ -10,19 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Orion")]
-[assembly: AssemblyCopyright("Copyright © Mikazuki 2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("4B7B61B1-959B-4EE7-BD7A-81FFBB9A6054")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,7 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
+[assembly: ComVisible(false)]
