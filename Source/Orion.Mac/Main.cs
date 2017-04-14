711f7daf9f974c9c7db11e1ac8925ffbed76de8a
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-04-14 18:47:25 +0900

    :+1: Add macOS project

---
+++ Source/Orion.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Orion.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
