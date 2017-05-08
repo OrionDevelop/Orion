1bd47b723901ce424aa0c8e9868f8403a8270184
Mikazuki <mikazuki_fuyuno@outlook.com>
2017-05-08 23:29:51 +0900

    :construction: Status actions.

--- Source/Orion.UWP/Views/Contents/StatusContent.xaml.cs
+++ Source/Orion.UWP/Views/Contents/StatusContent.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
+using WinRTXamlToolkit.Controls.Extensions;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -19,9 +10,58 @@ namespace Orion.UWP.Views.Contents
 {
     public sealed partial class StatusContent : UserControl
     {
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(StatusContent), new PropertyMetadata(false, PropertyChangedCallback));
+
+        public bool IsSelected
+        {
+            get => (bool) GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+
         public StatusContent()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Loaded += OnLoaded;
+            AppBar.Visibility = Visibility.Collapsed;
+            AppBar.Height = 0;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var root = this.GetFirstAncestorOfType<ListViewItem>();
+            if (root == null)
+                return;
+
+            SetBinding(IsSelectedProperty, new Binding
+            {
+                Source = root,
+                Path = new PropertyPath(nameof(root.IsSelected)),
+                Mode = BindingMode.TwoWay
+            });
+        }
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            if ((bool) args.NewValue)
+                (dependencyObject as StatusContent)?.ExpandCommandBar();
+            else
+                (dependencyObject as StatusContent)?.ContractCommandBar();
+        }
+
+        private void ContractCommandBar()
+        {
+            if (RootPanel.ActualHeight > 40)
+                RootPanel.Height = RootPanel.ActualHeight - 40;
+            AppBar.Visibility = Visibility.Collapsed;
+            AppBar.Height = 0;
+        }
+
+        private void ExpandCommandBar()
+        {
+            RootPanel.Height = RootPanel.ActualHeight + 40;
+            AppBar.Visibility = Visibility.Visible;
+            AppBar.Height = 40;
         }
     }
 }
